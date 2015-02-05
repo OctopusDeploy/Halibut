15b77be199b93e61c6c5b07b91567fc0d5533724
Paul Stovell <paul@paulstovell.com>
2015-02-05 11:46:12 +1000

    Don't test the polling tentacle so much, make the tests faster

--- source/Halibut.Tests/UsageFixture.cs
+++ source/Halibut.Tests/UsageFixture.cs
@@ -64,7 +64,7 @@ namespace Halibut.Tests
                 tentaclePolling.Poll(new Uri("poll://SQ-TENTAPOLL"), new ServiceEndPoint(new Uri("https://localhost:" + octopusPort), Certificates.OctopusPublicThumbprint));
 
                 var echo = octopus.CreateClient<IEchoService>("poll://SQ-TENTAPOLL", Certificates.TentaclePollingPublicThumbprint);
-                for (var i = 0; i < 2000; i++)
+                for (var i = 0; i < 100; i++)
                 {
                     Assert.That(echo.SayHello("Deploy package A"), Is.EqualTo("Deploy package A..."));
                 }
