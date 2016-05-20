6b983523483e6b705e9dfdc25dd06834294cee3b
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-05-20 13:54:09 +1000

    Convert to use GitVersion

---
+++ source/Solution Items/SolutionInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Halibut is a communication library for Octopus Deploy.")]
+[assembly: AssemblyCompany("Octopus Deploy Pty. Ltd.")]
+[assembly: AssemblyProduct("Halibut")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy Pty. Ltd. 2011-2015")]
+[assembly: AssemblyCulture("")]
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
