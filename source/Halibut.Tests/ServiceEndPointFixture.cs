ebb47d6fede2cbfd93b9fd9d30c3521cdd6b4714
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-06-01 14:43:52 +1000

    Add a test to make sure we can safely deserialize legacy ServiceEndpoints that don't have a proxy object

---
+++ source/Halibut.Tests/ServiceEndPointFixture.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Halibut.Tests
+{
+    [TestFixture]
+    public class ServiceEndPointFixture
+    {
+        [Test]
+        public void IncompleteEndPointCanBeDeserialized()
+        {
+            var json = "{BaseUri: \"http://google.com\", RemoteThumbprint: \"AAAA\"}";
+            var result = JsonConvert.DeserializeObject<ServiceEndPoint>(json);
+
+            Assert.AreEqual("google.com", result.BaseUri.Host);
+            Assert.AreEqual("AAAA", result.RemoteThumbprint);
+            Assert.Null(result.Proxy);
+        }
+    }
+}
