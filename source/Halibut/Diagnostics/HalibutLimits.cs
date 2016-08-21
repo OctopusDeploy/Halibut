d1868c1392655bcf97cd856d9ff3ebbb74ffe62e
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-08-22 06:48:34 +1000

    Add back appconfig for net450. new stuff drags in too many deps

--- source/Halibut/Diagnostics/HalibutLimits.cs
+++ source/Halibut/Diagnostics/HalibutLimits.cs
@@ -12,13 +12,21 @@ namespace Halibut.Diagnostics
     {
         static HalibutLimits()
         {
+#if NET40
+            var settings = System.Configuration.ConfigurationManager.AppSettings;
+
+            var fields = typeof (HalibutLimits).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
+            foreach (var field in fields)
+            {
+                var value = settings.Get("Halibut." + field.Name);
+                if (string.IsNullOrWhiteSpace(value)) continue;
+                var time = TimeSpan.Parse(value);
+                field.SetValue(null, time);
+            }
+#else
             // should be able to use Directory.GetCurrentDirectory()
             // to get the working path, but the nunit test runner
             // runs from another directory. Go with dll location for now.
-#if NET40
-            var directory = Path.GetDirectoryName(new Uri(typeof(HalibutLimits).Assembly.CodeBase).LocalPath);
-            //TODO
-#else
             var directory = Path.GetDirectoryName(new Uri(typeof(HalibutLimits).GetTypeInfo().Assembly.CodeBase).LocalPath);
             var builder = new ConfigurationBuilder();
             builder.SetBasePath(directory);
