b66b22f2591d93f96c49c14838d78c08c0fdbc06
Robert Wagner <robert.wagner@octopus.com>
2016-12-22 14:56:16 +1000

    Ignored the warning as it is intentional

--- source/Halibut/Transport/SecureListener.cs
+++ source/Halibut/Transport/SecureListener.cs
@@ -79,7 +79,9 @@ namespace Halibut.Transport
                     try
                     {
                         var client = await listener.AcceptTcpClientAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                         Task.Run(() => HandleClient(client));
+#pragma warning restore CS4014
                     }
                     catch (SocketException e) when (e.SocketErrorCode == SocketError.Interrupted)
                     {
