6f0810824c266eab8d0964a021203c0d64a7c580
Michael Compton <michael.compton@octopus.com>
2017-11-19 22:54:33 +1100

    sleep less

--- source/Halibut/Transport/SecureClient.cs
+++ source/Halibut/Transport/SecureClient.cs
@@ -150,7 +150,7 @@ namespace Halibut.Transport
             var stream = client.GetStream();
 
             log.Write(EventType.Security, "I feel sleepy ... Going to sleep");
-            Thread.Sleep(10000);
+            Thread.Sleep(3000);
             log.Write(EventType.Security, "Hi Matt, did your source load for you ???");
             log.Write(EventType.Security, "If you did, this file should be auto loaded from GitHub when you debug into it");
 
