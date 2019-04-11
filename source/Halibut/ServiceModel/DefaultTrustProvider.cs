611c90b523be83527d21287a1edd12aadf36a7c9
Vincent Parrett <vincent@finalbuilder.com>
2019-04-11 11:10:17 +1000

    Added ITrustProvider and default implementation

---
+++ source/Halibut/ServiceModel/DefaultTrustProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Halibut.ServiceModel
+{
+    internal class DefaultTrustProvider : ITrustProvider
+    {
+        readonly HashSet<string> trustedThumbprints = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        public void Add(string clientThumbprint)
+        {
+            lock (trustedThumbprints)
+                trustedThumbprints.Add(clientThumbprint);
+        }
+
+        public bool IsTrusted(string clientThumbprint)
+        {
+            lock (trustedThumbprints)
+                return trustedThumbprints.Contains(clientThumbprint);
+        }
+
+        public void Remove(string clientThumbprint)
+        {
+            lock (trustedThumbprints)
+                trustedThumbprints.Remove(clientThumbprint);
+        }
+
+        public void TrustOnly(IReadOnlyList<string> thumbprints)
+        {
+            lock (trustedThumbprints)
+            {
+                trustedThumbprints.Clear();
+                foreach (var thumbprint in thumbprints)
+                    trustedThumbprints.Add(thumbprint);
+            }
+        }
+
+        public string[] ToArray()
+        {
+            lock (trustedThumbprints)
+            {
+                return trustedThumbprints.ToArray();
+            }
+        }
+    }
+}
