3f0a6504221cdabc8deaf4e4b325dcc9380d90af
Vincent Parrett <vincent@finalbuilder.com>
2019-05-14 13:53:11 +1000

    Update source/Halibut/Transport/SecureListener.cs
    Better parameter names
    
    Co-Authored-By: Matt Richardson <matt.richardson@octopus.com>

--- source/Halibut/Transport/SecureListener.cs
+++ source/Halibut/Transport/SecureListener.cs
@@ -61,7 +61,7 @@ namespace Halibut.Transport
         }
 
         public SecureListener(IPEndPoint endPoint, X509Certificate2 serverCertificate, Func<MessageExchangeProtocol, Task> protocolHandler, Predicate<string> verifyClientThumbprint, ILogFactory logFactory, Func<string> getFriendlyHtmlPageContent, Func<Dictionary<string, string>> getFriendlyHtmlPageHeaders) :
-            this(endPoint, serverCertificate, protocolHandler, verifyClientThumbprint, logFactory, getFriendlyHtmlPageContent, getFriendlyHtmlPageHeaders, (x, y) => false)
+            this(endPoint, serverCertificate, protocolHandler, verifyClientThumbprint, logFactory, getFriendlyHtmlPageContent, getFriendlyHtmlPageHeaders, (clientName, thumbprint) => false)
         {
         }
 
