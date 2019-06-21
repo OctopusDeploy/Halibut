77766ccdd8a7e7d9ec2a4c8faeef45eeb47ed85f
Rob <robert.erez@gmail.com>
2019-06-22 09:45:49 +1000

    Add some discovery tests

---
+++ source/Halibut.Tests/DiscoveryClientFixture.cs
@@ -0,0 +1,55 @@
+using System;
+using FluentAssertions;
+using Halibut.Diagnostics;
+using Halibut.ServiceModel;
+using Halibut.Tests.TestServices;
+using Halibut.Transport;
+using NUnit.Framework;
+
+namespace Halibut.Tests
+{
+    public class DiscoveryClientFixture : IDisposable
+    {
+        ServiceEndPoint endpoint;
+        HalibutRuntime tentacle;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var services = new DelegateServiceFactory();
+            services.Register<IEchoService>(() => new EchoService());
+            tentacle = new HalibutRuntime(services, Certificates.TentacleListening);
+            var tentaclePort = tentacle.Listen();
+            tentacle.Trust(Certificates.OctopusPublicThumbprint);
+            endpoint = new ServiceEndPoint("https://localhost:" + tentaclePort, Certificates.TentacleListeningPublicThumbprint)
+            {
+                ConnectionErrorRetryTimeout = TimeSpan.MaxValue
+            };
+        }
+
+        public void Dispose()
+        {
+            tentacle.Dispose();
+        }
+
+
+        [Test]
+        public void DiscoverMethodReturnsEndpointDetails()
+        {
+            var client = new DiscoveryClient();
+            var discovered = client.Discover(new ServiceEndPoint(endpoint.BaseUri, ""));
+
+            discovered.RemoteThumbprint.ShouldBeEquivalentTo(endpoint.RemoteThumbprint);
+            discovered.BaseUri.ShouldBeEquivalentTo(endpoint.BaseUri);
+        }
+
+        [Test]
+        public void DiscoveringNonExistantEndpointThrows()
+        {
+            var client = new DiscoveryClient();
+            var fakeEndpoint = new ServiceEndPoint("https://fake-tentacle.example", "");
+
+            Assert.Throws<HalibutClientException>(() => client.Discover(fakeEndpoint), "No such host is known");
+        }
+    }
+}
