f841a317c6091abcfcd03b1ba0ea0205723c1849
Shane <tothegills@gmail.com>
2019-09-13 10:56:44 +1000

    Approve CreateConnectedTcpClient

--- source/Halibut.Tests/PublicSurfaceAreaFixture.ThePublicSurfaceAreaShouldNotRegress.NETCore.approved.cs
+++ source/Halibut.Tests/PublicSurfaceAreaFixture.ThePublicSurfaceAreaShouldNotRegress.NETCore.approved.cs
@@ -412,6 +412,7 @@ namespace Halibut.Transport
     public class TcpConnectionFactory : Halibut.Transport.IConnectionFactory
     {
         public TcpConnectionFactory(X509Certificate2 clientCertificate) { }
+        public static TcpClient CreateConnectedTcpClient(Halibut.ServiceEndPoint endPoint, Halibut.Diagnostics.ILog log) { }
         public Halibut.Transport.IConnection EstablishNewConnection(Halibut.ServiceEndPoint serviceEndpoint, Halibut.Diagnostics.ILog log) { }
     }
 }
