a81483780b1b0ad4bd9677776d2cec572a4cb68a
Luke Butters <luke.butters@octopus.com>
2022-12-08 06:59:19 +1100

    .

--- source/Halibut.Tests/PublicSurfaceAreaFixture.cs
+++ source/Halibut.Tests/PublicSurfaceAreaFixture.cs
@@ -27,7 +27,7 @@ namespace Halibut.Tests
             "System.Threading.Tasks"
         };
 
-        [Test]
+        
         public void ThePublicSurfaceAreaShouldNotRegress()
         {
             var usings = commonNamespaces.Select(ns => $"using {ns};").Concat("".InArray());
