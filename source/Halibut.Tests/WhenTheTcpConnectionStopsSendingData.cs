776b9baf668016add4b855845efe040b681bef09
Nathan Willoughby <nathan.willoughby@octopus.com>
2023-04-13 12:59:47 +1000

    Added method to pause port forwarder connections and close all connections

--- source/Halibut.Tests/WhenTheTcpConnectionStopsSendingData.cs
+++ source/Halibut.Tests/WhenTheTcpConnectionStopsSendingData.cs
@@ -16,26 +16,23 @@ namespace Halibut.Tests
         {
             var services = new DelegateServiceFactory();
             services.Register<IEchoService>(() => new EchoService());
-            
+
             using (var octopus = new HalibutRuntime(Certificates.Octopus))
             using (var tentacleListening = new HalibutRuntime(services, Certificates.TentacleListening))
             {
                 var tentaclePort = tentacleListening.Listen();
-                using (var loadBalancer = new PortForwarder(new Uri("https://localhost:" + tentaclePort), TimeSpan.Zero))
+                using (var portForwarder = new PortForwarder(new Uri("https://localhost:" + tentaclePort), TimeSpan.Zero))
                 {
                     tentacleListening.Trust(Certificates.OctopusPublicThumbprint);
-                
+
                     var data = new byte[1024];
                     new Random().NextBytes(data);
 
-                    var echo = octopus.CreateClient<IEchoService>("https://localhost:" + loadBalancer.PublicEndpoint.Port, Certificates.TentacleListeningPublicThumbprint);
+                    var echo = octopus.CreateClient<IEchoService>("https://localhost:" + portForwarder.PublicEndpoint.Port, Certificates.TentacleListeningPublicThumbprint);
 
                     echo.SayHello("Bob");
 
-                    foreach (var portForwarder in loadBalancer.Pumps)
-                    {
-                        portForwarder.Pause();
-                    }
+                    portForwarder.PauseExistingConnections();
 
                     var sayHelloTask = Task.Run(() => echo.SayHello("Bob"));
 
