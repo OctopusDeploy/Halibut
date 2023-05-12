c41e51b1363582693673dc8a0416b3eb4fdd45ad
Luke Butters <luke.butters@octopus.com>
2023-05-12 12:18:42 +1000

    .

--- source/Halibut.TestUtils.CompatBinary.Base/ProgramBase.cs
+++ source/Halibut.TestUtils.CompatBinary.Base/ProgramBase.cs
@@ -1,35 +1,38 @@
+using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 using Halibut.ServiceModel;
 using Halibut.TestUtils.SampleProgram.v5_0_429;
 
-namespace Halibut.TestUtils.SampleProgram.Base;
-
-public class ProgramBase
+namespace Halibut.TestUtils.SampleProgram.Base
 {
-    public static int Main(string[] args)
+    public class ProgramBase
     {
-        var tentacleCertPath = Environment.GetEnvironmentVariable("tentaclecertpath");
-        Console.WriteLine($"Using tentacle cert path: {tentacleCertPath}");
-        var TentacleCert = new X509Certificate2(tentacleCertPath);
-
-        var octopusThumbprint = Environment.GetEnvironmentVariable("octopusthumbprint");
-        Console.WriteLine($"Using octopus thumbprint: {octopusThumbprint}");
-
-        var addressToPoll = Environment.GetEnvironmentVariable("octopusservercommsport");
-        Console.WriteLine($"Will poll: {addressToPoll}");
-
-        var services = new DelegateServiceFactory();
-        services.Register<IEchoService>(() => new EchoService());
-        using (var tentaclePolling = new HalibutRuntime(services, TentacleCert))
+        public static int Main(string[] args)
         {
-            tentaclePolling.Poll(new Uri("poll://SQ-TENTAPOLL"), new ServiceEndPoint(new Uri(addressToPoll), octopusThumbprint));
+            var tentacleCertPath = Environment.GetEnvironmentVariable("tentaclecertpath");
+            Console.WriteLine($"Using tentacle cert path: {tentacleCertPath}");
+            var TentacleCert = new X509Certificate2(tentacleCertPath);
 
-            Console.WriteLine("RunningAndReady");
-            Console.WriteLine("Will Now sleep");
-            Console.Out.Flush();
-            Thread.Sleep(1000000);
+            var octopusThumbprint = Environment.GetEnvironmentVariable("octopusthumbprint");
+            Console.WriteLine($"Using octopus thumbprint: {octopusThumbprint}");
+
+            var addressToPoll = Environment.GetEnvironmentVariable("octopusservercommsport");
+            Console.WriteLine($"Will poll: {addressToPoll}");
+
+            var services = new DelegateServiceFactory();
+            services.Register<IEchoService>(() => new EchoService());
+            using (var tentaclePolling = new HalibutRuntime(services, TentacleCert))
+            {
+                tentaclePolling.Poll(new Uri("poll://SQ-TENTAPOLL"), new ServiceEndPoint(new Uri(addressToPoll), octopusThumbprint));
+
+                Console.WriteLine("RunningAndReady");
+                Console.WriteLine("Will Now sleep");
+                Console.Out.Flush();
+                Thread.Sleep(1000000);
+            }
+
+            return 1;
         }
-
-        return 1;
     }
 }
