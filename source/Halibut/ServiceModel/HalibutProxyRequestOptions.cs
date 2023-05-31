125c6fc4414039c2909a8758897b054ad63bef86
Luke Butters <luke.butters@octopus.com>
2023-05-31 10:20:05 +1000

    Add doco and add the new method to the halibut interface

--- source/Halibut/ServiceModel/HalibutProxyRequestOptions.cs
+++ source/Halibut/ServiceModel/HalibutProxyRequestOptions.cs
@@ -6,6 +6,10 @@ namespace Halibut.ServiceModel
 {
     public class HalibutProxyRequestOptions
     {
+        /// <summary>
+        /// When cancelled, this will only stop a RPC call if it is known to not be
+        /// received by the service.
+        /// </summary>
         public CancellationToken? ConnectCancellationToken { get; }
 
         public HalibutProxyRequestOptions(CancellationToken? connectCancellationToken)
