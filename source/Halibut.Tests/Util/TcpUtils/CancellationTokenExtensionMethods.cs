d143a165787f0346ed1ef2540ab524dbe7e7b0fb
Luke Butters <luke.butters@octopus.com>
2023-06-08 12:39:08 +1000

    Add the astask methods

---
+++ source/Halibut.Tests/Util/TcpUtils/CancellationTokenExtensionMethods.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Halibut.Tests.Util.TcpUtils
+{
+    internal static class CancellationTokenExtensionMethods
+    {
+        public static Task<TResult> AsTask<TResult>(this CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<TResult>();
+
+            IDisposable registration = null;
+            registration = cancellationToken.Register(() =>
+            {
+                tcs.TrySetCanceled();
+                registration?.Dispose();
+            }, useSynchronizationContext: false);
+
+            return tcs.Task;
+        }
+
+        public static Task AsTask(this CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<VoidResult>();
+
+            IDisposable registration = null;
+            registration = cancellationToken.Register(() =>
+            {
+                tcs.TrySetCanceled();
+                registration?.Dispose();
+            }, useSynchronizationContext: false);
+
+            return tcs.Task;
+        }
+
+        private struct VoidResult { }
+    }
+}
