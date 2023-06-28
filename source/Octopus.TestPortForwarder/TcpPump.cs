b08c885c2bb0cc6f7e304127b1b6ca30427f371f
Luke Butters <luke.butters@octopus.com>
2023-06-28 05:25:32 +0000

    What pump is that? (#252)
    * What pump is that? Give pumps an ID

--- source/Octopus.TestPortForwarder/TcpPump.cs
+++ source/Octopus.TestPortForwarder/TcpPump.cs
@@ -11,6 +11,12 @@ namespace Octopus.TestPortForwarder
 {
     public class TcpPump : IDisposable
     {
+        static long numberOfTcpPumpsCreated = 0;
+
+        static long NextTcpPumpNumber()
+        {
+            return Interlocked.Increment(ref numberOfTcpPumpsCreated);
+        }
         readonly Socket clientSocket;
         readonly EndPoint clientEndPoint;
         readonly Socket originSocket;
@@ -23,6 +29,7 @@ namespace Octopus.TestPortForwarder
         bool isDisposed;
         public bool IsPaused { get; set; }
         private Func<BiDirectionalDataTransferObserver> factory;
+        public long PumpNumber { get; } = NextTcpPumpNumber();
 
         public TcpPump(Socket clientSocket, Socket originSocket, EndPoint originEndPoint, TimeSpan sendDelay, Func<BiDirectionalDataTransferObserver> factory, int numberOfBytesToDelaySending, ILogger logger)
         {
@@ -35,7 +42,7 @@ namespace Octopus.TestPortForwarder
             this.numberOfBytesToDelaySending = numberOfBytesToDelaySending;
             clientEndPoint = clientSocket.RemoteEndPoint ?? throw new ArgumentException("Remote endpoint is null", nameof(clientSocket));
         }
-
+        
         public event EventHandler<EventArgs> Stopped;
 
         public void Start()
