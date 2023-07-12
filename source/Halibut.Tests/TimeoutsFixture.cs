172b08c2d673b458f88d46b0de5fe28f2200b9b5
Luke Butters <luke.butters@octopus.com>
2023-07-12 12:55:20 +1000

    .

--- source/Halibut.Tests/TimeoutsFixture.cs
+++ source/Halibut.Tests/TimeoutsFixture.cs
@@ -6,9 +6,7 @@ using Halibut.Diagnostics;
 using Halibut.Tests.Support;
 using Halibut.Tests.Support.TestAttributes;
 using Halibut.Tests.TestServices;
-using Halibut.Tests.Util;
 using NUnit.Framework;
-using Octopus.TestPortForwarder;
 
 namespace Halibut.Tests
 {
@@ -16,9 +14,8 @@ namespace Halibut.Tests
     {
         [Test]
         [TestCaseSource(typeof(ServiceConnectionTypesToTest))]
-        public async Task ReadingARequestMes(ServiceConnectionType serviceConnectionType)
+        public async Task ReadingARequestMessage(ServiceConnectionType serviceConnectionType)
         {
-            
             using (var clientAndService = await ClientServiceBuilder
                        .ForServiceConnectionType(serviceConnectionType)
                        .WithPortForwarding(out var portForwarderRef)
@@ -29,20 +26,19 @@ namespace Halibut.Tests
                 portForwarderRef.Value = clientAndService.PortForwarder;
                 var echo = clientAndService.CreateClient<IEchoService>();
                 echo.SayHello("Make a request to make sure the connection is running, and ready. Lets not measure SSL setup cost.");
-                
+
                 var pauseConnections = clientAndService.CreateClient<IDoSomeActionService>(point =>
                 {
                     // We don't want to measure the polling queue timeouts.
                     point.PollingRequestMaximumMessageProcessingTimeout = TimeSpan.FromMinutes(10);
                     point.PollingRequestQueueTimeout = TimeSpan.FromMinutes(10);
                 });
-                
+
                 var sw = Stopwatch.StartNew();
                 var e = Assert.Throws<HalibutClientException>(() => pauseConnections.Action());
                 sw.Stop();
                 new SerilogLoggerBuilder().Build().Error(e, "msg");
                 sw.Elapsed.Should().BeCloseTo(HalibutLimits.TcpClientReceiveTimeout, TimeSpan.FromSeconds(5));
-
             }
         }
     }
