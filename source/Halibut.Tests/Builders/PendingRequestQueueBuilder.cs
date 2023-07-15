74fb0321d970d0f2155c70c7c5d4770015ad1120
Stephen Burman <91929764+sburmanoctopus@users.noreply.github.com>
2023-07-15 15:05:40 +1000

    Tests for PendingRequestQueue (#299)
    Tests for PendingRequestQueue

---
+++ source/Halibut.Tests/Builders/PendingRequestQueueBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Halibut.ServiceModel;
+using Halibut.Diagnostics;
+
+namespace Halibut.Tests.Builders
+{
+    public class PendingRequestQueueBuilder
+    {
+        string? endpoint;
+        TimeSpan? pollingQueueWaitTimeout;
+
+        public PendingRequestQueueBuilder WithEndpoint(string endpoint)
+        {
+            this.endpoint = endpoint;
+            return this;
+        }
+
+        public PendingRequestQueueBuilder WithPollingQueueWaitTimeout(TimeSpan? pollingQueueWaitTimeout)
+        {
+            this.pollingQueueWaitTimeout = pollingQueueWaitTimeout;
+            return this;
+        }
+
+        public PendingRequestQueue Build()
+        {
+            var endpoint = this.endpoint ?? "poll://endpoint001";
+            var pollingQueueWaitTimeout = this.pollingQueueWaitTimeout ?? HalibutLimits.PollingQueueWaitTimeout;
+
+            var log = new InMemoryConnectionLog(endpoint);
+
+            var pendingRequestQueue = new PendingRequestQueue(log, pollingQueueWaitTimeout);
+            return pendingRequestQueue;
+        }
+    }
+}
---
+++ source/Halibut.Tests/Builders/ResponseMessageBuilder.cs
@@ -0,0 +1,30 @@
+﻿using Halibut.Transport.Protocol;
+
+namespace Halibut.Tests.Builders
+{
+    public class ResponseMessageBuilder
+    {
+        readonly string id;
+
+        public ResponseMessageBuilder(string id)
+        {
+            this.id = id;
+        }
+
+        public static ResponseMessageBuilder FromRequest(RequestMessage requestMessage)
+        {
+            var responseMessageBuilder = new ResponseMessageBuilder(requestMessage.Id);
+            return responseMessageBuilder;
+        }
+
+        public ResponseMessage Build()
+        {
+            var response = new ResponseMessage
+            {
+                Id = id,
+                Result = new object()
+            };
+            return response;
+        }
+    }
+}
