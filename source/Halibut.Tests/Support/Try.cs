1059bf37946e82290a4cda5313f3a617a81313ca
Luke Butters <luke.butters@octopus.com>
2023-07-19 14:48:27 +1000

    .

--- source/Halibut.Tests/Support/Try.cs
+++ source/Halibut.Tests/Support/Try.cs
@@ -4,8 +4,16 @@ namespace Halibut.Tests.Support
 {
     public static class Try
     {
-        public static void CatchingError(Action tryThisAction, Action<Exception> onFailure) {
-        
+        public static void CatchingError(Action tryThisAction, Action<Exception> onFailure) 
+        {
+            try
+            {
+                tryThisAction();
+            }
+            catch (Exception e)
+            {
+                onFailure(e);
+            }        
         }
     }
 }
