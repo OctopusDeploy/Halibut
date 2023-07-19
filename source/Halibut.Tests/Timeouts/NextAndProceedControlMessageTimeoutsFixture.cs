de44301fc1ae54648b15902c5e96fcee5657e25d
Luke Butters <luke.butters@octopus.com>
2023-07-19 15:30:51 +1000

    Dont keep pausing the connection

--- source/Halibut.Tests/Timeouts/NextAndProceedControlMessageTimeoutsFixture.cs
+++ source/Halibut.Tests/Timeouts/NextAndProceedControlMessageTimeoutsFixture.cs
@@ -33,6 +33,7 @@ namespace Halibut.Tests.Timeouts
                                {
                                    Logger.Information("Pausing pump since a write of size {Size} was received", stream.Length);
                                    tcpPump.Pause();
+                                   pauseStreamWhenServiceSendsMessageOfSize = null;
                                }
                            })
                            .Build())
@@ -42,25 +43,26 @@ namespace Halibut.Tests.Timeouts
             {
                 var echo = clientAndService.CreateClient<IEchoService>();
                 
-                    echo.SayHello(Some.RandomAsciiStringOfLength(2000));
-                    // --> NEXT sent
-                    // --> Proceed sent
-                    await Task.Delay(TimeSpan.FromSeconds(3)); // Allow enough time for the polling queue to unnecessarily send a null message down the queue because of a race condition in the queue. 
-                    long nextMessageSize = dataSentSizes.Last();
-                    pauseStreamWhenServiceSendsMessageOfSize = nextMessageSize; // Lets hope they are all the same size
+                echo.SayHello(Some.RandomAsciiStringOfLength(2000));
+                // --> NEXT sent
+                // --> Proceed sent
+                await Task.Delay(TimeSpan.FromSeconds(3)); // Allow enough time for the polling queue to unnecessarily send a null message down the queue because of a race condition in the queue. 
+                long nextMessageSize = dataSentSizes.Last();
+                pauseStreamWhenServiceSendsMessageOfSize = nextMessageSize; // Lets hope they are all the same size
+                Logger.Information("Will pause the pump next time the polling service sends a message of size {Size}", pauseStreamWhenServiceSendsMessageOfSize);
 
-                    echo.SayHello(Some.RandomAsciiStringOfLength(2000)); // Second last message
-                    // --> NEXT sent and pump paused here.
-                    
-                    var sw = Stopwatch.StartNew();
-                    // Service must detect that it can't get a PROCEED back in time and so must decide to abandon and then try again within
-                    // its retry policy.
-                    echo.SayHello(Some.RandomAsciiStringOfLength(2000));
-                    sw.Stop();
-                    Logger.Information("It took: " + sw.Elapsed);
-                    sw.Elapsed.Should().BeGreaterThanOrEqualTo(HalibutLimits.TcpClientHeartbeatReceiveTimeout - TimeSpan.FromSeconds(2)) // -2s since tentacle will begin its countdown on the read which may start just after
-                                                                                                                                         // the response to the 'Second last message' is put back into the queue.
-                        .And.BeLessThan(HalibutLimits.TcpClientReceiveTimeout, "Service should not be using this timeout to detect the control message is not coming back, it should use the shorter one.");
+                echo.SayHello(Some.RandomAsciiStringOfLength(2000)); // Second last message
+                // --> NEXT sent and pump paused here.
+                
+                var sw = Stopwatch.StartNew();
+                // Service must detect that it can't get a PROCEED back in time and so must decide to abandon and then try again within
+                // its retry policy.
+                echo.SayHello(Some.RandomAsciiStringOfLength(2000));
+                sw.Stop();
+                Logger.Information("It took: " + sw.Elapsed);
+                sw.Elapsed.Should().BeGreaterThanOrEqualTo(HalibutLimits.TcpClientHeartbeatReceiveTimeout - TimeSpan.FromSeconds(2)) // -2s since tentacle will begin its countdown on the read which may start just after
+                                                                                                                                     // the response to the 'Second last message' is put back into the queue.
+                    .And.BeLessThan(HalibutLimits.TcpClientReceiveTimeout, "Service should not be using this timeout to detect the control message is not coming back, it should use the shorter one.");
             }
         }
     }
