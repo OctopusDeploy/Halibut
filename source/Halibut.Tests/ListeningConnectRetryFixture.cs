3c434a72ca577ac97026ea34414c9eb27da52354
Luke Butters <luke.butters@octopus.com>
2023-07-20 11:40:29 +1000

    .

--- source/Halibut.Tests/ListeningConnectRetryFixture.cs
+++ source/Halibut.Tests/ListeningConnectRetryFixture.cs
@@ -96,17 +96,54 @@ namespace Halibut.Tests
 
                 var echoService = clientAndService.CreateClient<IEchoService>(point =>
                 {
-                    point.RetryListeningSleepInterval = TimeSpan.FromSeconds(1);
+                    point.RetryListeningSleepInterval = TimeSpan.FromSeconds(10);
                     point.ConnectionErrorRetryTimeout = TimeSpan.MaxValue;
-                    point.RetryCountLimit = 15;
+                    point.RetryCountLimit = 4;
                 });
 
                 var sw = Stopwatch.StartNew();
                 Assert.Throws<HalibutClientException>(() => echoService.SayHello("hello"));
                 sw.Stop();
 
-                
-                sw.Elapsed.Should().BeCloseTo(TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(7)/* Give a big amount of leeway */);
+                // Expected ~30s since we sleep 10s _between_ each attempt.
+                sw.Elapsed.Should().BeCloseTo(TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(15)/* Give a big amount of leeway for windows */);
+            }
+        }
+        
+        [Test]
+        public async Task ListeningRetriesAttemptsCanEventuallyWork()
+        {
+            TcpConnectionsCreatedCounter tcpConnectionsCreatedCounter = null;
+            using (var clientAndService = await LatestClientAndLatestServiceBuilder.Listening()
+                       .WithPortForwarding(port =>
+                       {
+                           var portForwarder = PortForwarderUtil.ForwardingToLocalPort(port)
+                               .WithCountTcpConnectionsCreated(out tcpConnectionsCreatedCounter)
+                               .Build();
+
+                           return portForwarder;
+                       })
+                       .WithEchoService()
+                       .Build(CancellationToken))
+            {
+                clientAndService.PortForwarder!.EnterKillNewAndExistingConnectionsMode();
+
+                var echoService = clientAndService.CreateClient<IEchoService>(point =>
+                {
+                    point.RetryListeningSleepInterval = TimeSpan.FromSeconds(1);
+                    point.ConnectionErrorRetryTimeout = TimeSpan.MaxValue;
+                    point.RetryCountLimit = 999999;
+                });
+
+                var echoCallThatShouldEventuallySucced = Task.Run(() => echoService.SayHello("hello"));
+                while (tcpConnectionsCreatedCounter.ConnectionsCreatedCount < 5)
+                {
+                    Logger.Information("TCP count is at: {Count}", tcpConnectionsCreatedCounter.ConnectionsCreatedCount);
+                    await Task.Delay(TimeSpan.FromSeconds(1), CancellationToken);
+                }
+                clientAndService.PortForwarder.ReturnToNormalMode();
+
+                await echoCallThatShouldEventuallySucced;
             }
         }
     }
