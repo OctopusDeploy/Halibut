d2df571637843430710a3d803c86e49b04486d26
Adrian Codrington <adrian.codrington@octopus.com>
2023-07-21 12:00:51 +0800

    Revert to info logging level

--- source/Halibut.Tests/ParallelRequestsFixture.cs
+++ source/Halibut.Tests/ParallelRequestsFixture.cs
@@ -19,48 +19,47 @@ namespace Halibut.Tests
         public async Task SendMessagesToTentacleInParallel(ClientAndServiceTestCase clientAndServiceTestCase)
         {
             using var clientAndService = await clientAndServiceTestCase.CreateTestCaseBuilder()
-                .WithHalibutLoggingLevel(LogLevel.Trace)
+                .WithHalibutLoggingLevel(LogLevel.Info)
                 .WithStandardServices()
                 .Build(CancellationToken);
+            
+            var readDataSteamService = clientAndService.CreateClient<IReadDataStreamService>();
+
+            var dataStreams = CreateDataStreams();
+
+            var messagesAreSentTheSameTimeSemaphore = new SemaphoreSlim(0, dataStreams.Length);
+
+            int threadCount = 64;
+            int threadCompletionCount = 0;
+            var threads = new List<Thread>();
+            var exceptions = new ConcurrentBag<Exception>();
+            for (var i = 0; i < threadCount; i++)
             {
-                var readDataSteamService = clientAndService.CreateClient<IReadDataStreamService>();
-
-                var dataStreams = CreateDataStreams();
-
-                var messagesAreSentTheSameTimeSemaphore = new SemaphoreSlim(0, dataStreams.Length);
-
-                int threadCount = 64;
-                int threadCompletionCount = 0;
-                var threads = new List<Thread>();
-                var exceptions = new ConcurrentBag<Exception>();
-                for (var i = 0; i < threadCount; i++)
+                var thread = new Thread(() =>
                 {
-                    var thread = new Thread(() =>
+                    // Gotta handle exceptions when running in a 
+                    // Thread, or you're gonna have a bad time.
+                    try
                     {
-                        // Gotta handle exceptions when running in a 
-                        // Thread, or you're gonna have a bad time.
-                        try
-                        {
-                            messagesAreSentTheSameTimeSemaphore.Wait(CancellationToken);
-                            var received = readDataSteamService.SendData(dataStreams);
-                            received.Should().Be(5 * dataStreams.Length);
-                            Interlocked.Increment(ref threadCompletionCount);
-                        }
-                        catch (Exception e)
-                        {
-                            exceptions.Add(e);
-                        }
-                    });
-                    thread.Start();
-                    threads.Add(thread);
-                }
-
-                messagesAreSentTheSameTimeSemaphore.Release(dataStreams.Length);
-
-                WaitForAllThreads(threads);
-                exceptions.Should().BeEmpty();
-                threadCompletionCount.Should().Be(threadCount);
+                        messagesAreSentTheSameTimeSemaphore.Wait(CancellationToken);
+                        var received = readDataSteamService.SendData(dataStreams);
+                        received.Should().Be(5 * dataStreams.Length);
+                        Interlocked.Increment(ref threadCompletionCount);
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions.Add(e);
+                    }
+                });
+                thread.Start();
+                threads.Add(thread);
             }
+
+            messagesAreSentTheSameTimeSemaphore.Release(dataStreams.Length);
+
+            WaitForAllThreads(threads);
+            exceptions.Should().BeEmpty();
+            threadCompletionCount.Should().Be(threadCount);
         }
 
         static DataStream[] CreateDataStreams()
