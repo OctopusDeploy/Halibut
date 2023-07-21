9fc4ec86e64581ef40bd1af12aaf8c8b0930d8c5
Luke Butters <luke.butters@octopus.com>
2023-07-21 13:18:57 +1000

    .

---
+++ source/Halibut.Tests/Support/Wait.cs
@@ -0,0 +1,18 @@
+using System;
+using System.DirectoryServices.ActiveDirectory;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Halibut.Tests.Support
+{
+    public static class Wait
+    {
+        public static async Task For(Func<bool> toBecomeTrue, CancellationToken cancellationToken)
+        {
+            while (!toBecomeTrue())
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(20), cancellationToken);
+            }
+        }
+    }
+}
