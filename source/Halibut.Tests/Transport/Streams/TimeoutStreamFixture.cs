4a3a6a37150476b54bddd0b823dd81082a9cb22a
Nathan Willoughby <86938706+nathanwoctopusdeploy@users.noreply.github.com>
2023-08-02 00:46:48 -0700

    Add a stream wrapper that provides consistent cancellation and timeout for Async Read and Async Write operations across net48 and net6.0 (#368)

---
+++ source/Halibut.Tests/Transport/Streams/TimeoutStreamFixture.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Halibut.Tests.Support;
+using Halibut.Tests.Support.TestAttributes;
+using Halibut.Transport.Streams;
+using NUnit.Framework;
+
+namespace Halibut.Tests.Transport.Streams
+{
+    [TestTimeout(timeoutInSeconds: 20)]
+    public class TimeoutStreamFixture : BaseTest
+    {
+        [Test]
+        public async Task ReadShouldPassThrough()
+        {
+            var (disposables, sut, performListenerWrite) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+            using (disposables)
+            {
+                await performListenerWrite("Test");
+
+                var buffer = new byte[19];
+                var readBytes = sut.Read(buffer, 0, 19);
+                var readData = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                
+                Assert.AreEqual("Test", readData);
+            }
+        }
+        
+        [Test]
+        public async Task ReadAsyncShouldPassThrough()
+        {
+            var (disposables, sut, performListenerWrite) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+            using (disposables)
+            {
+                await performListenerWrite("Test");
+
+                var buffer = new byte[19];
+                var readBytes = await sut.ReadAsync(buffer, 0, 19, CancellationToken);
+                var readData = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                
+                Assert.AreEqual("Test", readData);
+            }
+        }
+
+        [Test]
+        public async Task ReadAsyncShouldCancel()
+        {
+            using (var readTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5)))
+            {
+                var (disposables, sut, _) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+                using (disposables)
+                {
+                    // Ensure the timeouts are not used
+                    sut.WriteTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+                    sut.ReadTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+                    
+                    var stopWatch = Stopwatch.StartNew();
+                    
+                    var actualException = await Try.CatchingError(async () => await sut.ReadAsync(new byte[19], 0, 19, readTokenSource.Token));
+                    
+                    stopWatch.Stop();
+
+                    actualException.Should().NotBeNull().And.BeOfType<OperationCanceledException>();
+                    actualException!.Message.Should().Be("The ReadAsync operation was cancelled.");
+
+                    stopWatch.Elapsed.Should().BeLessThan(TimeSpan.FromSeconds(10));
+                }
+            }
+        }
+
+        [Test]
+        public async Task ReadAsyncShouldTimeout()
+        {
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+            using (disposables)
+            {
+                // Ensure the correct timeout is used
+                sut.WriteTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+                sut.ReadTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+
+                var stopWatch = Stopwatch.StartNew();
+
+                var actualException = await Try.CatchingError(async () => await sut.ReadAsync(new byte[19], 0, 19, CancellationToken));
+                
+                stopWatch.Stop();
+
+                actualException.Should().NotBeNull().And.BeOfType<OperationCanceledException>();
+                actualException!.Message.Should().Be("The ReadAsync operation timed out after 00:00:05.");
+
+                stopWatch.Elapsed.Should().BeLessThan(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        [Test]
+        public async Task WriteShouldPassThrough()
+        {
+            string? readData = null;
+            
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(
+                CancellationToken,
+                onListenerRead: async data =>
+                {
+                    await Task.CompletedTask;
+                    readData += data;
+                });
+
+            using (disposables)
+            {
+                var buffer = Encoding.UTF8.GetBytes("Test");
+                sut.Write(buffer, 0, buffer.Length);
+
+                while ((readData?.Length ?? 0) < 4 && !CancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(10, CancellationToken);
+                }
+
+                Assert.AreEqual("Test", readData);
+            }
+        }
+        
+        [Test]
+        public async Task WriteAsyncShouldPassThrough()
+        {
+            string? readData = null;
+            
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(
+                CancellationToken,
+                onListenerRead: async data =>
+                {
+                    await Task.CompletedTask;
+                    readData += data;
+                });
+
+            using (disposables)
+            {
+                var buffer = Encoding.UTF8.GetBytes("Test");
+                await sut.WriteAsync(buffer, 0, buffer.Length, CancellationToken);
+
+                while ((readData?.Length ?? 0) < 4 && !CancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(10, CancellationToken);
+                }
+
+                Assert.AreEqual("Test", readData);
+            }
+        }
+
+        [Test]
+        public async Task WriteAsyncShouldCancel()
+        {
+            using (var writeTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5)))
+            {
+                var (disposables, sut, _) = await BuildTcpClientAndTcpListener(
+                    CancellationToken,
+                    onListenerRead: async _ => await DelayForeverToTryAndDelayWriting(CancellationToken));
+
+                using (disposables)
+                {
+                    // Ensure the timeouts are not used
+                    sut.WriteTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+                    sut.ReadTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+
+                    var data = new byte[655360];
+                    var r = new Random();
+                    r.NextBytes(data);
+
+                    var stopWatch = Stopwatch.StartNew();
+                    
+                    // Brute force attempt to get the Write to be slow
+                    var actualException = await Try.RunTillExceptionOrCancellation(
+                        async () => await sut.WriteAsync(data, 0, data.Length, writeTokenSource.Token),
+                        CancellationToken);
+                    
+                    stopWatch.Stop();
+
+                    actualException.Should().NotBeNull().And.BeOfType<OperationCanceledException>();
+                    actualException!.Message.Should().Be("The WriteAsync operation was cancelled.");
+
+                    stopWatch.Elapsed.Should().BeLessThan(TimeSpan.FromSeconds(10));
+                }
+            }
+        }
+
+        [Test]
+        public async Task WriteAsyncShouldTimeout()
+        {
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(
+                CancellationToken, 
+                onListenerRead: async _ => await DelayForeverToTryAndDelayWriting(CancellationToken));
+
+            using (disposables)
+            {
+                sut.WriteTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+                // Ensure the correct timeout is used
+                sut.ReadTimeout = (int)TimeSpan.FromSeconds(120).TotalMilliseconds;
+
+                var data = new byte[655360];
+                var r = new Random();
+                r.NextBytes(data);
+
+                var stopWatch = Stopwatch.StartNew();
+
+                // Brute force attempt to get the Write to be slow
+                var actualException = await Try.RunTillExceptionOrCancellation(
+                    async () => await sut.WriteAsync(data, 0, data.Length, CancellationToken), 
+                    CancellationToken);
+
+                stopWatch.Stop();
+
+                actualException.Should().NotBeNull().And.BeOfType<OperationCanceledException>();
+                actualException!.Message.Should().Be("The WriteAsync operation timed out after 00:00:05.");
+
+                stopWatch.Elapsed.Should().BeLessThan(TimeSpan.FromSeconds(10));
+            }
+        }
+
+        [Test]
+        public async Task CloseShouldPassThrough()
+        {
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+            using (disposables)
+            {
+                sut.Close();
+
+                Action read = () => sut.Read(new byte[1], 0, 1);
+                read.Should().Throw<ObjectDisposedException>("Because the stream is closed");
+            }
+        }
+
+        [Test]
+        public async Task DisposeShouldPassThrough()
+        {
+            var (disposables, sut, _) = await BuildTcpClientAndTcpListener(CancellationToken);
+
+            using (disposables)
+            {
+                sut.Dispose();
+
+                Action read = () => sut.Read(new byte[1], 0, 1);
+                read.Should().Throw<ObjectDisposedException>("Because the stream is closed");
+            }
+        }
+
+        static async Task DelayForeverToTryAndDelayWriting(CancellationToken cancellationToken)
+        {
+            await Task.Delay(-1, cancellationToken);
+        }
+
+        async Task<(IDisposable Disposables, Stream SystemUnderTest, Func<string, Task> PerformServiceWriteFunc)> BuildTcpClientAndTcpListener(
+            CancellationToken cancellationToken, 
+            Func<string, Task>? onListenerRead = null)
+        {
+            Func<string, Task>? performServiceWriteFunc = null;
+            var disposableCollection = new DisposableCollection();
+
+            var service = new TcpListener(IPAddress.Loopback, 0); 
+            service.Start();
+
+            var _ = Task.Run(async () =>
+            {
+                using var serviceTcpClient = await service.AcceptTcpClientAsync();
+                serviceTcpClient.ReceiveBufferSize = 10;
+                serviceTcpClient.SendBufferSize = 10;
+
+                using var serviceStream = serviceTcpClient.GetStream();
+                performServiceWriteFunc = async data => await serviceStream.WriteAsync(Encoding.UTF8.GetBytes(data), 0, data.Length, cancellationToken);
+                
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var buffer = new byte[19];
+                    var readBytes = await serviceStream.ReadAsync(buffer, 0, 19, cancellationToken);
+                    
+                    var readData = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                    if (onListenerRead != null)
+                    {
+                        await onListenerRead(readData);
+                    }
+                }
+            }, cancellationToken);
+            
+            var client = new TcpClient();
+            client.ReceiveBufferSize = 10;
+            client.SendBufferSize = 10;
+
+            await client.ConnectAsync("localhost", ((IPEndPoint)service.LocalEndpoint).Port);
+            
+            var clientStream = client.GetStream();
+            disposableCollection.Add(clientStream);
+
+            var sut = new TimeoutStream(clientStream);
+            disposableCollection.Add(sut);
+
+            while (performServiceWriteFunc == null)
+            { 
+                await Task.Delay(10, cancellationToken);
+            }
+
+            return (disposableCollection, sut, performServiceWriteFunc!);
+        }
+    }
+}
---
+++ source/Halibut/Transport/Streams/TimeoutStream.cs
@@ -0,0 +1,192 @@
+﻿#nullable enable
+using System;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Threading;
+using System.Threading.Tasks;
+using Halibut.Util;
+
+namespace Halibut.Transport.Streams
+{
+    class TimeoutStream : Stream
+    {
+        readonly Stream inner;
+
+        public TimeoutStream(Stream inner)
+        {
+            this.inner = inner;
+        }
+
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            await WrapWithCancellationAndTimeout(
+                async ct =>
+                {
+                    await inner.FlushAsync(ct);
+                    return 0;
+                },
+                CanTimeout ? WriteTimeout : int.MaxValue,
+                nameof(FlushAsync),
+                cancellationToken);
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return await WrapWithCancellationAndTimeout(
+                async ct => await inner.ReadAsync(buffer, offset, count, ct),
+                CanTimeout ? ReadTimeout : int.MaxValue,
+                nameof(ReadAsync),
+                cancellationToken);
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            await WrapWithCancellationAndTimeout(
+                async ct =>
+                {
+                    await inner.WriteAsync(buffer, offset, count, ct);
+                    return 0;
+                },
+                CanTimeout ? WriteTimeout : int.MaxValue,
+                nameof(WriteAsync),
+                cancellationToken);
+        }
+
+#if !NETFRAMEWORK
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            return await WrapWithCancellationAndTimeout(
+                async ct => await inner.ReadAsync(buffer, ct),
+                CanTimeout ? ReadTimeout : int.MaxValue,
+                nameof(ReadAsync),
+                cancellationToken);
+        }
+
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            await WrapWithCancellationAndTimeout(
+                async ct =>
+                {
+                    await inner.WriteAsync(buffer, ct);
+                    return 0;
+                },
+                CanTimeout ? WriteTimeout : int.MaxValue,
+                nameof(WriteAsync),
+                cancellationToken);
+        }
+#endif
+
+        async Task<T> WrapWithCancellationAndTimeout<T>(Func<CancellationToken, Task<T>> action, int timeout, string methodName, CancellationToken cancellationToken)
+        {
+            using var timeoutCancellationTokenSource = new CancellationTokenSource(timeout);
+            using var linkedCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, timeoutCancellationTokenSource.Token);
+
+            var actionTask = action(linkedCancellationTokenSource.Token);
+            var cancellationTask = linkedCancellationTokenSource.Token.AsTask<T>();
+
+            try
+            {
+                var completedTask = await Task.WhenAny(actionTask, cancellationTask);
+
+                if (completedTask == cancellationTask)
+                {
+                    actionTask.IgnoreUnobservedExceptions();
+
+                    try
+                    {
+                        inner.Close();
+                    }
+                    catch { }
+
+                    ThrowMeaningfulException();
+                }
+
+                return await actionTask;
+            }
+            catch (Exception e)
+            {
+                ThrowMeaningfulException(e);
+
+                throw;
+            }
+
+            void ThrowMeaningfulException(Exception? innerException = null)
+            {
+                if (timeoutCancellationTokenSource.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException($"The {methodName} operation timed out after {TimeSpan.FromMilliseconds(timeout)}.", innerException);
+                }
+
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException($"The {methodName} operation was cancelled.", innerException);
+                }
+            }
+        }
+
+        public override void Close() => inner.Close();
+
+        public override async Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken) => await inner.CopyToAsync(destination, bufferSize, cancellationToken);
+
+        public override int EndRead(IAsyncResult asyncResult) => inner.EndRead(asyncResult);
+
+        public override void EndWrite(IAsyncResult asyncResult) => inner.EndWrite(asyncResult);
+
+        public override int ReadByte() => inner.ReadByte();
+
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state) => inner.BeginRead(buffer, offset, count, callback, state);
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state) => inner.BeginWrite(buffer, offset, count, callback, state);
+
+        public override void Flush() => inner.Flush();
+
+        public override int Read(byte[] buffer, int offset, int count) => inner.Read(buffer, offset, count);
+
+        public override long Seek(long offset, SeekOrigin origin) => inner.Seek(offset, origin);
+
+        public override void SetLength(long value) => inner.SetLength(value);
+
+        public override void Write(byte[] buffer, int offset, int count) => inner.Write(buffer, offset, count);
+
+        public override void WriteByte(byte value) => inner.WriteByte(value);
+
+#if !NETFRAMEWORK
+        public override void CopyTo(Stream destination, int bufferSize) => inner.CopyTo(destination, bufferSize);
+        
+        public override async ValueTask DisposeAsync() => await inner.DisposeAsync();
+        
+        public override int Read(Span<byte> buffer) => inner.Read(buffer);
+
+        public override void Write(ReadOnlySpan<byte> buffer) => inner.Write(buffer);
+#endif
+
+#if NETFRAMEWORK
+        public override ObjRef CreateObjRef(Type requestedType) => inner.CreateObjRef(requestedType);
+
+        public override object? InitializeLifetimeService() => inner.InitializeLifetimeService();
+#endif
+
+        public override int ReadTimeout
+        {
+            get => inner.ReadTimeout;
+            set => inner.ReadTimeout = value;
+        }
+
+        public override int WriteTimeout
+        {
+            get => inner.WriteTimeout;
+            set => inner.WriteTimeout = value;
+        }
+
+        public override bool CanTimeout => inner.CanTimeout;
+        public override bool CanRead => inner.CanRead;
+        public override bool CanSeek => inner.CanSeek;
+        public override bool CanWrite => inner.CanWrite;
+        public override long Length => inner.Length;
+        public override long Position
+        {
+            get => inner.Position;
+            set => inner.Position = value;
+        }
+    }
+}
