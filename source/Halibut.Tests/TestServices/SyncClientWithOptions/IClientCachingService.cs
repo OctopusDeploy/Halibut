92445d25f39d427dc456d460c6d26bdd6c4ac0fb
Luke Butters <luke.butters@octopus.com>
2023-08-07 15:30:31 +1000

    Test ResponseMessageCacheFixture in async and sync (#390)
    * Test ResponseMessageCacheFixture in async and sync

---
+++ source/Halibut.Tests/TestServices/SyncClientWithOptions/IClientCachingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Halibut.ServiceModel;
+using Halibut.Transport.Caching;
+
+namespace Halibut.Tests.TestServices.SyncClientWithOptions
+{
+    public interface IClientCachingService
+    {
+        Guid NonCachableCall(HalibutProxyRequestOptions halibutProxyRequestOptions);
+
+        [CacheResponse(600)]
+        Guid CachableCall(HalibutProxyRequestOptions halibutProxyRequestOptions);
+    }
+}
