91248e721ca504b46523183f6c5bdcad8889888d
Adrian Codrington <277700+acodrington@users.noreply.github.com>
2023-08-08 08:22:03 +0800

    Convert WhenCallingServicesSimilarToTheOnesInTentacle to use async code path (#398)

---
+++ source/Halibut.Tests/TestServices/Async/IAsyncClientCapabilitiesServiceV2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octopus.Tentacle.Contracts.Capabilities;
+
+namespace Halibut.Tests.TestServices.Async
+{
+    public interface IAsyncClientCapabilitiesServiceV2
+    {
+        public Task<CapabilitiesResponseV2> GetCapabilitiesAsync();
+    }
+}
---
+++ source/Halibut.Tests/TestServices/Async/IAsyncClientScriptServiceV2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Octopus.Tentacle.Contracts.ScriptServiceV2;
+
+namespace Halibut.Tests.TestServices.Async
+{
+    public interface IAsyncClientScriptServiceV2
+    {
+        Task<ScriptStatusResponseV2> StartScriptAsync(StartScriptCommandV2 command);
+        Task<ScriptStatusResponseV2> GetStatusAsync(ScriptStatusRequestV2 request);
+        Task<ScriptStatusResponseV2> CancelScriptAsync(CancelScriptCommandV2 command);
+        Task CompleteScriptAsync(CompleteScriptCommandV2 command);
+    }
+}
