732a155fae076bc926d7e11b920bc3c36ba1b986
Luke Butters <luke.butters@octopus.com>
2023-08-09 11:53:43 +1000

    Make PollingClient support async

--- source/Halibut/Transport/PollingClient.cs
+++ source/Halibut/Transport/PollingClient.cs
@@ -1,5 +1,7 @@
+#nullable enable
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Halibut.Diagnostics;
 using Halibut.ServiceModel;
 using Halibut.Transport.Protocol;
@@ -13,47 +15,66 @@ namespace Halibut.Transport
         readonly ILog log;
         readonly ISecureClient secureClient;
         readonly Uri subscription;
-        readonly Thread thread;
-        readonly CancellationToken cancellationToken;
+        Thread? pollingClientLoopThread;
         bool working;
+
+        Task? pollingClientLoopTask;
+        readonly CancellationTokenSource workingCancellationTokenSource;
+        readonly CancellationToken cancellationToken;
+        
         readonly Func<RetryPolicy> createRetryPolicy;
+        readonly AsyncHalibutFeature asyncHalibutFeature;
+        
 
-        [Obsolete("Use the overload that provides a logger. This remains for backwards compatibility.")]
-        public PollingClient(Uri subscription, ISecureClient secureClient, Func<RequestMessage, ResponseMessage> handleIncomingRequest, Func<RetryPolicy> createRetryPolicy)
-            : this(subscription, secureClient, handleIncomingRequest, null, createRetryPolicy)
-        {
-        }
-
-        public PollingClient(Uri subscription, ISecureClient secureClient, Func<RequestMessage, ResponseMessage> handleIncomingRequest, ILog log, Func<RetryPolicy> createRetryPolicy)
-            : this(subscription, secureClient, handleIncomingRequest, log, CancellationToken.None, createRetryPolicy)
-        {
-        }
-
-        public PollingClient(Uri subscription, ISecureClient secureClient, Func<RequestMessage, ResponseMessage> handleIncomingRequest, ILog log, CancellationToken cancellationToken, Func<RetryPolicy> createRetryPolicy)
+        public PollingClient(Uri subscription, ISecureClient secureClient, Func<RequestMessage, ResponseMessage> handleIncomingRequest, ILog log, CancellationToken cancellationToken, Func<RetryPolicy> createRetryPolicy, AsyncHalibutFeature asyncHalibutFeature)
         {
             this.subscription = subscription;
             this.secureClient = secureClient;
             this.handleIncomingRequest = handleIncomingRequest;
             this.log = log;
             this.cancellationToken = cancellationToken;
+            workingCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
             this.createRetryPolicy = createRetryPolicy;
-            thread = new Thread(ExecutePollingLoop);
-            thread.Name = "Polling client for " + secureClient.ServiceEndpoint + " for subscription " + subscription;
-            thread.IsBackground = true;
+            this.asyncHalibutFeature = asyncHalibutFeature;
+            
         }
 
         public void Start()
         {
             working = true;
-            thread.Start();
+
+            if (asyncHalibutFeature == AsyncHalibutFeature.Disabled)
+            {
+                pollingClientLoopThread = new Thread(ExecutePollingLoop!);
+                pollingClientLoopThread.Name = "Polling client for " + secureClient.ServiceEndpoint + " for subscription " + subscription;
+                pollingClientLoopThread.IsBackground = true;
+            }
+            else
+            {
+                pollingClientLoopTask = Task.Run(async () => await ExecutePollingLoopAsync());
+            }
         }
 
         public void Dispose()
         {
             working = false;
-        }
+            try
+            {
+                workingCancellationTokenSource.Cancel();
+            }
+            catch
+            {
+            }
 
-        // TODO: ASYNC ME UP!
+            try
+            {
+                workingCancellationTokenSource.Dispose();
+            }
+            catch
+            {
+            }
+        }
+        
         void ExecutePollingLoop(object ignored)
         {
             var retry = createRetryPolicy();
@@ -92,6 +113,46 @@ namespace Halibut.Transport
                 finally
                 {
                     Thread.Sleep(sleepFor);
+                }
+            }
+        }
+        
+        async Task ExecutePollingLoopAsync()
+        {
+            var retry = createRetryPolicy();
+            var sleepFor = TimeSpan.Zero;
+            while (!workingCancellationTokenSource.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    try
+                    {
+                        retry.Try();
+                        await secureClient.ExecuteTransactionAsync(async (protocol, cancellationToken) =>
+                        {
+                            // We have successfully connected at this point so reset the retry policy
+                            // Subsequent connection issues will try and reconnect quickly and then back-off
+                            retry.Success();
+                            await protocol.ExchangeAsSubscriberAsync(subscription, handleIncomingRequest, int.MaxValue, cancellationToken);
+                        }, workingCancellationTokenSource.Token);
+                        retry.Success();
+                    }
+                    finally
+                    {
+                        sleepFor = retry.GetSleepPeriod();
+                    }
+                }
+                catch (HalibutClientException ex)
+                {
+                    log?.WriteException(EventType.Error, $"Halibut client exception: {ex.Message?.TrimEnd('.')}. Retrying in {sleepFor.TotalSeconds:n1} seconds", ex);
+                }
+                catch (Exception ex)
+                {
+                    log?.WriteException(EventType.Error, $"Exception in the polling loop. Retrying in {sleepFor.TotalSeconds:n1} seconds. This may be cause by a network error and usually rectifies itself. Disregard this message unless you are having communication problems.", ex);
+                }
+                finally
+                {
+                    await Task.Delay(sleepFor, workingCancellationTokenSource.Token);
                 }
             }
         }
