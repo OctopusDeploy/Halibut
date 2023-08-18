faa95c242772b6f972d8297b724a1f68b58d26fd
Luke Butters <luke.butters@octopus.com>
2023-08-18 17:45:05 +1000

    Don't fall into a sync read when the end of stream was found (#444)
    * Don't fall into a sync read when the end of stream was found

--- source/Halibut/Transport/Protocol/MessageSerializer.cs
+++ source/Halibut/Transport/Protocol/MessageSerializer.cs
@@ -195,7 +195,7 @@ namespace Halibut.Transport.Protocol
             }
         }
         
-        async Task<T> ReadCompressedMessageAsync<T>(Stream stream, IRewindableBuffer rewindableBuffer, CancellationToken cancellationToken)
+        async Task<T> ReadCompressedMessageAsync<T>(ErrorRecordingStream stream, IRewindableBuffer rewindableBuffer, CancellationToken cancellationToken)
         {
             rewindableBuffer.StartBuffer();
             try
@@ -209,6 +209,16 @@ namespace Halibut.Transport.Protocol
 
                 using var deflatedInMemoryStream = new ReadIntoMemoryBufferStream(decompressedByteCountingStream, readIntoMemoryLimitBytes, OnDispose.LeaveInputStreamOpen);
                 await deflatedInMemoryStream.BufferIntoMemoryFromSourceStreamUntilLimitReached(cancellationToken);
+                
+                // If the end of stream was found and we read nothing from the streams
+                if (stream.WasTheEndOfStreamEncountered && compressedByteCountingStream.BytesRead == 0 && decompressedByteCountingStream.BytesRead == 0)
+                {
+                    // When this happens we would normally continue to the BsonDataReader which would
+                    // do a sync read(), to find that the stream had ended. We avoid that sync call by
+                    // short circuiting to what would happen which is:  
+                    // The BsonReader would return a non null MessageEnvelope with a null message, which is what we do here.
+                    return new MessageEnvelope<T>().Message; // And hack around we can't return null
+                }
 
                 using (var bson = new BsonDataReader(deflatedInMemoryStream) { CloseInput = false })
                 {
