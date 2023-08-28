093984403625ca3b500fe1f12e8831a5fc7953ba
Adrian Codrington <adrian.codrington@octopus.com>
2023-08-28 11:18:32 +0800

    Add TcpClient.CloseImmediately to TestProxy

---
+++ source/Halibut.TestProxy/TcpClientExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Halibut.TestProxy
+{
+    public static class TcpClientExtensionMethods
+    {
+        public static void CloseImmediately(this TcpClient client)
+        {
+            client.CloseImmediately(_ => { });
+        }
+
+        public static void CloseImmediately(this TcpClient client, Action<Exception> onError)
+        {
+            Try.CatchingError(() => client.Client.Close(0), onError);
+            Try.CatchingError(client.Close, onError);
+        }
+    }
+}
