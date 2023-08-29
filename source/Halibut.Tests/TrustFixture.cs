ebb292bfc9f1da44aa7a8056881e670469023956
Adrian Codrington <adrian.codrington@octopus.com>
2023-08-29 12:49:02 +0800

    Add test for TrustOnly

---
+++ source/Halibut.Tests/TrustFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Halibut.Tests.Support;
+using Halibut.Tests.Support.TestAttributes;
+using Halibut.Tests.Support.TestCases;
+using Halibut.Tests.TestServices.Async;
+using Halibut.TestUtils.Contracts;
+using NUnit.Framework;
+
+namespace Halibut.Tests
+{
+    public class TrustFixture : BaseTest
+    {
+        [Test]
+        [LatestClientAndLatestServiceTestCases(testListening: true, testPolling: false, testWebSocket: false)]
+        public async Task TrustOnlyClosesConnectionsToUntrustedClients(ClientAndServiceTestCase clientAndServiceTestCase)
+        {
+            await using var clientAndService = await clientAndServiceTestCase.CreateTestCaseBuilder()
+                .AsLatestClientAndLatestServiceBuilder()
+                .WithStandardServices()
+                .Build(CancellationToken);
+
+            var echoService = clientAndService.CreateClient<IEchoService, IAsyncClientEchoService>();
+            var result = await echoService.SayHelloAsync("Hello");
+            result.Should().Be("Hello...");
+
+            // Trust no one
+            clientAndService.Service!.TrustOnly(Array.Empty<string>());
+
+            await AssertAsync.Throws<Exception>(async () => await echoService.SayHelloAsync("Hello again"));
+        }
+    }
+}
