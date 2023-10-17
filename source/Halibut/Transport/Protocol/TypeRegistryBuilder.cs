79e80332dd08883e22e33159e33cbe8252baafbe
Alastair Pitts <alastair.pitts@octopus.com>
2023-10-18 09:51:06 +1100

    Add support for derived types to the default TypeRegistry

---
+++ source/Halibut/Transport/Protocol/TypeRegistryBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Halibut.Transport.Protocol
+{
+    public class TypeRegistryBuilder
+    {
+        Assembly[] typeAssemblies;
+
+        /// <summary>
+        /// Adds a list of assemblies to the <see cref="ITypeRegistry"/>. Used to search for contract types for derived types.
+        /// </summary>
+        /// <param name="assemblies">An array of <see cref="Assembly">Assemblies</see> to add to the <see cref="ITypeRegistry"/>.</param>
+        public TypeRegistryBuilder WithTypeAssemblies(params Assembly[] assemblies)
+        {
+            typeAssemblies = assemblies;
+            return this;
+        }
+
+        public ITypeRegistry Build()
+        {
+            return new TypeRegistry(typeAssemblies);
+        }
+    }
+}
