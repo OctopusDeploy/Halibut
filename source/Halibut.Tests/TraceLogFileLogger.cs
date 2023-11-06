71ac22c1e5c5da5fef9d85d43f6b58b5d17e6f66
Luke Butters <luke.butters@octopus.com>
2023-11-06 14:23:52 +1100

    Wait for all logs to be written

--- source/Halibut.Tests/TraceLogFileLogger.cs
+++ source/Halibut.Tests/TraceLogFileLogger.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Threading;
 
@@ -10,18 +9,18 @@ namespace Halibut.Tests
 {
     public class TraceLogFileLogger : IDisposable
     {
-        readonly AsyncQueue<string> queue = new AsyncQueue<string>();
+        readonly AsyncQueue<string> queue = new();
         readonly string tempFilePath = Path.GetTempFileName();
         string testHash;
 
-        readonly TaskCompletionSource<bool> noMoreDelaysToWritingLogs;
-        readonly Task noMoreDelaysTask;
+        readonly TaskCompletionSource<bool> prepareForLogCollectionTaskCompletionSource;
+        readonly Task prepareForLogCollection;
         readonly Task writeDataToDiskTask;
 
         public TraceLogFileLogger()
         {
-            noMoreDelaysToWritingLogs = new TaskCompletionSource<bool>();
-            noMoreDelaysTask = noMoreDelaysToWritingLogs.Task;
+            prepareForLogCollectionTaskCompletionSource = new TaskCompletionSource<bool>();
+            prepareForLogCollection = prepareForLogCollectionTaskCompletionSource.Task;
             writeDataToDiskTask = WriteDataToFile();
         }
 
@@ -29,54 +28,42 @@ namespace Halibut.Tests
         {
             this.testHash = testHash;
         }
-        
+
         public void WriteLine(string logMessage)
         {
+            if (prepareForLogCollection.IsCompleted) return;
             queue.Enqueue(logMessage);
-        } 
-        
+        }
+
         async Task WriteDataToFile()
         {
-            while (true)
+            while (!prepareForLogCollection.IsCompleted)
             {
                 // Don't hammer the disk, let some log message queue up before writing them.
-                if (!noMoreDelaysTask.IsCompleted)
-                {
-                    await Task.WhenAny(Task.Delay(5), noMoreDelaysTask);
-                }
+                if (!prepareForLogCollection.IsCompleted) await Task.WhenAny(Task.Delay(5), prepareForLogCollection);
 
                 var list = new List<string>();
                 list.Add(await queue.DequeueAsync());
-                while (queue.TryDequeue(out var log))
-                {
-                    list.Add(log);
-                }
+                while (queue.TryDequeue(out var log)) list.Add(log);
 
                 using (var fileAppender = new StreamWriter(tempFilePath, true, Encoding.UTF8, 8192))
                 {
-                    foreach (var logLine in list)
-                    {
-                        await fileAppender.WriteLineAsync(logLine);
-                    }
+                    foreach (var logLine in list) await fileAppender.WriteLineAsync(logLine);
 
                     await fileAppender.FlushAsync();
                 }
             }
         }
-        
-        void AllowSomeTimeForTheQueueToEmpty()
+
+        void FinishWritingLogs()
         {
-            noMoreDelaysToWritingLogs.SetResult(false);
-            for (int i = 0; i < 100; i++)
-            {
-                if(queue.IsEmpty) break;
-                Thread.Sleep(1);
-            }
-        } 
+            prepareForLogCollectionTaskCompletionSource.SetResult(false);
+            writeDataToDiskTask.GetAwaiter().GetResult();
+        }
 
         public bool CopyLogFileToArtifacts()
         {
-            AllowSomeTimeForTheQueueToEmpty();
+            FinishWritingLogs();
             // The current directory is expected to have the following structure
             // (w/ variance depending on Debug/Release and dotnet framework used (net6.0, net48 etc):
             //
@@ -86,7 +73,7 @@ namespace Halibut.Tests
             // from which point we can navigate to the artifacts directory.
             var currentDirectory = Directory.GetCurrentDirectory();
             var rootDirectory = new DirectoryInfo(currentDirectory).Parent.Parent.Parent.Parent.Parent;
-            
+
             var traceLogsDirectory = rootDirectory.CreateSubdirectory("artifacts").CreateSubdirectory("trace-logs");
             var fileName = $"{testHash}.tracelog";
 
@@ -100,7 +87,7 @@ namespace Halibut.Tests
                 return false;
             }
         }
-        
+
         public void Dispose()
         {
             try
