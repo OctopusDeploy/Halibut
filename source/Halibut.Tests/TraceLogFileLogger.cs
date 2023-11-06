cbbb2848c6d98dfc11eda43999ef7bc8373e1965
Luke Butters <luke.butters@octopus.com>
2023-11-06 12:56:28 +1100

    Record all log lines, and don't let logging alter the test run

--- source/Halibut.Tests/TraceLogFileLogger.cs
+++ source/Halibut.Tests/TraceLogFileLogger.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Threading;
 
 namespace Halibut.Tests
 {
     public class TraceLogFileLogger : IDisposable
     {
-        string tempFilePath = Path.GetTempFileName();
+        readonly AsyncQueue<string> queue = new AsyncQueue<string>();
+        readonly string tempFilePath = Path.GetTempFileName();
         string testHash;
+
+        readonly TaskCompletionSource<bool> noMoreDelaysToWritingLogs;
+        readonly Task noMoreDelaysTask;
+        readonly Task writeDataToDiskTask;
+
+        public TraceLogFileLogger()
+        {
+            noMoreDelaysToWritingLogs = new TaskCompletionSource<bool>();
+            noMoreDelaysTask = noMoreDelaysToWritingLogs.Task;
+            writeDataToDiskTask = WriteDataToFile();
+        }
 
         public void SetTestHash(string testHash)
         {
@@ -15,11 +32,51 @@ namespace Halibut.Tests
         
         public void WriteLine(string logMessage)
         {
-            File.AppendAllLines(tempFilePath, new[] { logMessage });
+            queue.Enqueue(logMessage);
+        } 
+        
+        async Task WriteDataToFile()
+        {
+            while (true)
+            {
+                // Don't hammer the disk, let some log message queue up before writing them.
+                if (!noMoreDelaysTask.IsCompleted)
+                {
+                    await Task.WhenAny(Task.Delay(5), noMoreDelaysTask);
+                }
+
+                var list = new List<string>();
+                list.Add(await queue.DequeueAsync());
+                while (queue.TryDequeue(out var log))
+                {
+                    list.Add(log);
+                }
+
+                using (var fileAppender = new StreamWriter(tempFilePath, true, Encoding.UTF8, 8192))
+                {
+                    foreach (var logLine in list)
+                    {
+                        await fileAppender.WriteLineAsync(logLine);
+                    }
+
+                    await fileAppender.FlushAsync();
+                }
+            }
         }
+        
+        void AllowSomeTimeForTheQueueToEmpty()
+        {
+            noMoreDelaysToWritingLogs.SetResult(false);
+            for (int i = 0; i < 100; i++)
+            {
+                if(queue.IsEmpty) break;
+                Thread.Sleep(1);
+            }
+        } 
 
         public bool CopyLogFileToArtifacts()
         {
+            AllowSomeTimeForTheQueueToEmpty();
             // The current directory is expected to have the following structure
             // (w/ variance depending on Debug/Release and dotnet framework used (net6.0, net48 etc):
             //
