17a3584f56e5ab1abb97fd0fdc7be32180b32f21
Stephen Burman <stephen.burman@octopus.com>
2023-11-21 14:57:59 +1100

    Fixing impacted test

--- source/Halibut/Util/HalibutTimeoutsAndLimitsExtensionMethods.cs
+++ source/Halibut/Util/HalibutTimeoutsAndLimitsExtensionMethods.cs
@@ -5,13 +5,6 @@ namespace Halibut.Util
 {
     public static class HalibutTimeoutsAndLimitsExtensionMethods
     {
-        public static HalibutTimeoutsAndLimits WithTcpClientReceiveTimeout(this HalibutTimeoutsAndLimits halibutTimeoutsAndLimits, TimeSpan tcpClientReceiveTimeout)
-        {
-            halibutTimeoutsAndLimits.TcpClientTimeout = new(
-                sendTimeout: halibutTimeoutsAndLimits.TcpClientTimeout.SendTimeout, 
-                receiveTimeout: tcpClientReceiveTimeout);
-
-            return halibutTimeoutsAndLimits;
-        }
+        
     }
 }
