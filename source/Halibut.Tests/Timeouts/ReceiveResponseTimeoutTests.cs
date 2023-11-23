82a251256d89a8c98e868d3b31bbd017df4a8f82
Stephen Burman <stephen.burman@octopus.com>
2023-11-23 11:40:38 +1100

    Adding a test to ensure data streams respect TcpClientReceiveResponseTransmissionAfterInitialReadTimeout (#551)

--- source/Halibut.Tests/Timeouts/ReceiveResponseTimeoutTests.cs
+++ source/Halibut.Tests/Timeouts/ReceiveResponseTimeoutTests.cs
@@ -9,11 +9,12 @@ using Halibut.Tests.Support.TestAttributes;
 using Halibut.Tests.Support.TestCases;
 using Halibut.Tests.TestServices;
 using Halibut.Tests.TestServices.Async;
+using Halibut.Tests.Util;
 using Halibut.TestUtils.Contracts;
 using NUnit.Framework;
 using Octopus.TestPortForwarder;
 
-namespace Halibut.Tests
+namespace Halibut.Tests.Timeouts
 {
     public class ReceiveResponseTimeoutTests : BaseTest
     {
@@ -21,14 +22,11 @@ namespace Halibut.Tests
         [LatestClientAndLatestServiceTestCases(testNetworkConditions: false, testWebSocket: false)]
         public async Task WhenRpcExecutionExceedsReceiveResponseTimeout_ThenInitialDataReadShouldTimeout(ClientAndServiceTestCase clientAndServiceTestCase)
         {
-            var halibutTimeoutsAndLimits = new HalibutTimeoutsAndLimits
-            {
-                TcpClientReceiveResponseTimeout = TimeSpan.FromMilliseconds(100),
-                // Ensure subsequent reads do not trigger timeouts
-                TcpClientReceiveResponseTransmissionAfterInitialReadTimeout = TimeSpan.FromHours(1)
-            };
-
             // Arrange
+            var halibutTimeoutsAndLimits = new HalibutTimeoutsAndLimits();
+            halibutTimeoutsAndLimits.WithAllTcpTimeoutsTo(TimeSpan.FromHours(1));
+            halibutTimeoutsAndLimits.TcpClientReceiveResponseTimeout = TimeSpan.FromMilliseconds(100);
+            
             await using (var clientAndService = await clientAndServiceTestCase.CreateTestCaseBuilder()
                              .AsLatestClientAndLatestServiceBuilder()
                              .WithHalibutTimeoutsAndLimits(halibutTimeoutsAndLimits)
@@ -50,19 +48,16 @@ namespace Halibut.Tests
         public async Task WhenRpcExecutionIsWithinReceiveResponseTimeout_ButSubsequentDataIsDelayed_ThenTimeoutShouldOccur(ClientAndServiceTestCase clientAndServiceTestCase)
         {
             // Arrange
-            var halibutTimeoutsAndLimits = new HalibutTimeoutsAndLimits
-            {
-                // Ensure execution time does not trigger timeouts
-                TcpClientReceiveResponseTimeout = TimeSpan.FromHours(1),
-                TcpClientReceiveResponseTransmissionAfterInitialReadTimeout = TimeSpan.FromMilliseconds(100)
-            };
+            var halibutTimeoutsAndLimits = new HalibutTimeoutsAndLimits();
+            halibutTimeoutsAndLimits.WithAllTcpTimeoutsTo(TimeSpan.FromHours(1));
+            halibutTimeoutsAndLimits.TcpClientReceiveResponseTransmissionAfterInitialReadTimeout = TimeSpan.FromMilliseconds(100);
 
             var enoughDataToCauseMultipleReadOperations = Enumerable.Range(0, 1024 * 1024)
                 .Select(_ => Guid.NewGuid().ToString())
                 .ToList();
-            
+
             var listService = new AsyncListService(enoughDataToCauseMultipleReadOperations);
-            
+
             var dataTransferObserver = new DataTransferObserverBuilder()
                 .WithWritingDataObserver((_, _) =>
                 {
@@ -87,6 +82,35 @@ namespace Halibut.Tests
 
                 // Act
                 (await AssertionExtensions.Should(() => lastServiceClient.GetListAsync()).ThrowAsync<HalibutClientException>())
+                    .And.Message.Should().ContainAny(
+                        "Connection timed out.",
+                        "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond");
+            }
+        }
+        
+        [Test]
+        [LatestClientAndLatestServiceTestCases(testNetworkConditions: false, testWebSocket: false)]
+        public async Task WhenRpcExecutionIsWithinReceiveResponseTimeout_ButDataStreamDataIsDelayed_ThenTimeoutShouldOccur(ClientAndServiceTestCase clientAndServiceTestCase)
+        {
+            var halibutTimeoutsAndLimits = new HalibutTimeoutsAndLimits();
+            halibutTimeoutsAndLimits.WithAllTcpTimeoutsTo(TimeSpan.FromHours(1));
+            halibutTimeoutsAndLimits.TcpClientReceiveResponseTransmissionAfterInitialReadTimeout = TimeSpan.FromSeconds(1);
+
+            // Arrange
+            await using (var clientAndService = await clientAndServiceTestCase.CreateTestCaseBuilder()
+                             .AsLatestClientAndLatestServiceBuilder()
+                             .WithHalibutTimeoutsAndLimits(halibutTimeoutsAndLimits)
+                             .WithPortForwarding(out var portForwarderRef)
+                             .WithReturnSomeDataStreamService(() => DataStreamUtil.From(
+                                 firstSend: "hello",
+                                 andThenRun: portForwarderRef.Value!.PauseExistingConnections,
+                                 thenSend: "All done"))
+                             .Build(CancellationToken))
+            {
+                var returnDataStreamService = clientAndService.CreateAsyncClient<IReturnSomeDataStreamService, IAsyncClientReturnSomeDataStreamService>();
+
+                // Act
+                (await AssertionExtensions.Should(() => returnDataStreamService.SomeDataStreamAsync()).ThrowAsync<HalibutClientException>())
                     .And.Message.Should().ContainAny(
                         "Connection timed out.",
                         "A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond");
