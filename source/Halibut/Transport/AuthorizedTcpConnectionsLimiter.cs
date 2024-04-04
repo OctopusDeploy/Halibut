10e5f740389012fc74597f0472f71c8d431c567d
Alastair Pitts <alastair@pitts.io>
2024-04-05 10:39:30 +1100

    Fix concurrency issue when removing dictionary item

--- source/Halibut/Transport/AuthorizedTcpConnectionsLimiter.cs
+++ source/Halibut/Transport/AuthorizedTcpConnectionsLimiter.cs
@@ -14,10 +14,12 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Halibut.Diagnostics;
 using Halibut.Exceptions;
+using Halibut.Util;
 
 namespace Halibut.Transport
 {
@@ -31,7 +33,7 @@ namespace Halibut.Transport
         readonly HalibutTimeoutsAndLimits timeoutsAndLimits;
 
         //we use a StrongBox here so we can use Interlock to increment/decrement the value
-        ConcurrentDictionary<string, StrongBox<int>> authorizedConnectionCountPerThumbprint = new();
+        ConcurrentDictionary<string, ConnectionCount> authorizedConnectionCountPerThumbprint = new();
 
         public AuthorizedTcpConnectionsLimiter(HalibutTimeoutsAndLimits timeoutsAndLimits)
         {
@@ -46,23 +48,25 @@ namespace Halibut.Transport
         class AuthorizedTcpConnectionLease : IDisposable
         {
             readonly string thumbprint;
-            readonly ConcurrentDictionary<string, StrongBox<int>> authorizedConnectionCountPerThumbprint;
+            readonly ConcurrentDictionary<string, ConnectionCount> authorizedConnectionCountPerThumbprint;
 
-            public AuthorizedTcpConnectionLease(string thumbprint, ConcurrentDictionary<string, StrongBox<int>> authorizedConnectionCountPerThumbprint, int maximumAcceptedTcpConnectionsPerThumbprint)
+            public AuthorizedTcpConnectionLease(string thumbprint, ConcurrentDictionary<string, ConnectionCount> authorizedConnectionCountPerThumbprint, int maximumAcceptedTcpConnectionsPerThumbprint)
             {
                 this.thumbprint = thumbprint;
                 this.authorizedConnectionCountPerThumbprint = authorizedConnectionCountPerThumbprint;
 
-                var count = this.authorizedConnectionCountPerThumbprint.GetOrAdd(thumbprint, _ => new StrongBox<int>(0));
+                var count = this.authorizedConnectionCountPerThumbprint.GetOrAdd(thumbprint, _ => new ConnectionCount(0));
 
                 //increment the count via an interlock
-                var resultCount = Interlocked.Increment(ref count.Value);
+                var resultCount = Interlocked.Increment(ref count.Count);
 
-                //validate the new count. If this throws an exception, it'll
+                //validate the new count. If this throws an exception, it'll kill the connection
                 if (resultCount > maximumAcceptedTcpConnectionsPerThumbprint)
                 {
                     //decrement as this connection has been rejected
-                    Interlocked.Decrement(ref count.Value);
+                    Interlocked.Decrement(ref count.Count);
+
+                    //throw an exception, bailing on the connection
                     throw new AuthorizedTcpConnectionsExceededException($"Exceeded the maximum number ({maximumAcceptedTcpConnectionsPerThumbprint}) of authorised TCP connections for thumbprint {thumbprint}");
                 }
             }
@@ -72,14 +76,43 @@ namespace Halibut.Transport
                 if (authorizedConnectionCountPerThumbprint.TryGetValue(thumbprint, out var count))
                 {
                     //decrement the count of authorized connections
-                    var result = Interlocked.Decrement(ref count.Value);
+                    Interlocked.Decrement(ref count.Count);
 
-                    //if this was the last connection, remove the value from the dictionary
-                    if (result == 0)
-                    {
-                        authorizedConnectionCountPerThumbprint.TryRemove(thumbprint, out _);
-                    }
+                    // Remove the key from the dictionary if the value is 0
+                    // this will only remove the item if there is a value that matches this key & value
+                    // so if it's been incremented after the Decrement, then this will not find a match and not remove anything
+                    authorizedConnectionCountPerThumbprint.TryRemove(new KeyValuePair<string, ConnectionCount>(thumbprint, new ConnectionCount(0)));
                 }
+            }
+        }
+
+        class ConnectionCount : IEquatable<ConnectionCount>
+        {
+            public int Count;
+
+            public ConnectionCount(int count)
+            {
+                Count = count;
+            }
+
+            public bool Equals(ConnectionCount? other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return Count == other.Count;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((ConnectionCount)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return Count.GetHashCode();
             }
         }
     }
