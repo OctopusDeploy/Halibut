49d97d27f85528fe9eef1852a2f65371b6d25470
Rhys Parry <rhys.parry@octopus.com>
2024-11-18 11:22:32 +1000

    Remove outdated samples

--- source/Halibut.SampleClient/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Reflection;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("Halibut.SampleClient")]
