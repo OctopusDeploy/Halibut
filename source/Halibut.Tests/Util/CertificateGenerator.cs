86dcff1ea44ce9e084564ae3a10826776b0e336a
Geoff Battye <geoff.battye@octopus.com>
2025-02-11 15:01:25 +1100

    Removed commented-out code.

--- source/Halibut.Tests/Util/CertificateGenerator.cs
+++ source/Halibut.Tests/Util/CertificateGenerator.cs
@@ -13,11 +13,6 @@ namespace Halibut.Tests.Util
             var name = Guid.NewGuid().ToString();
             using var rsa = RSA.Create(2048);
             var request = new CertificateRequest($"CN={name}", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-
-            // request.CertificateExtensions.Add(new X509BasicConstraintsExtension(true, false, 0, true));
-            // request.CertificateExtensions.Add(new X509SubjectKeyIdentifierExtension(request.PublicKey, false));
-            // request.CertificateExtensions.Add(new X509KeyUsageExtension(X509KeyUsageFlags.DigitalSignature, false));
-
             var certificate = request.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
             var bytes = certificate.Export(X509ContentType.Pfx);
             var filePath = Path.Combine(folderPath, $"{name}.pfx");
