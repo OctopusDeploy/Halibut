65ab5ac178492b012573ff30690ac9d1bf10c4a1
Rhys Parry <rhys.parry@octopus.com>
2025-09-08 14:36:31 +1000

    Configure supported SSL via env variable

--- source/Halibut/Transport/SslConfiguration.cs
+++ source/Halibut/Transport/SslConfiguration.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Security.Authentication;
 
 namespace Halibut.Transport
 {
     public static class SslConfiguration
     {
-        public static SslProtocols SupportedProtocols
+        static SslProtocols GetSupportedProtocols()
         {
+            var supportedProtocolMode = Environment.GetEnvironmentVariable("HALIBUT_SUPPORTED_SSL_PROTOCOLS")?.ToLowerInvariant();
+
+            if (supportedProtocolMode == "legacy")
+            {
 #pragma warning disable SYSLIB0039
-            // See https://learn.microsoft.com/en-us/dotnet/fundamentals/syslib-diagnostics/syslib0039
-            // TLS 1.0 and 1.1 are obsolete from .NET 7
-            get => SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Tls13;
+                // See https://learn.microsoft.com/en-us/dotnet/fundamentals/syslib-diagnostics/syslib0039
+                // TLS 1.0 and 1.1 are obsolete from .NET 7
+                return SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12 | SslProtocols.Tls13;
 #pragma warning restore SYSLIB0039
+            }
+
+            if (supportedProtocolMode == "system")
+            {
+                return SslProtocols.None;
+            }
+
+            if (supportedProtocolMode == "modern")
+            {
+                return SslProtocols.Tls12 | SslProtocols.Tls13;
+            }
+
+            if (supportedProtocolMode == "tls1.3")
+            {
+                return SslProtocols.Tls13;
+            }
+
+            return SslProtocols.None;
         }
+
+        public static SslProtocols SupportedProtocols { get; } = GetSupportedProtocols();
     }
 }
