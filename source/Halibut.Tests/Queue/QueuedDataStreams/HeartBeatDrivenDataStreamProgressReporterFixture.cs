c85ae0f908360f8933b9b7c8c6043624467b6924
Luke Butters <luke.butters@octopus.com>
2025-09-09 10:59:38 +1000

    .

--- source/Halibut.Tests/Queue/QueuedDataStreams/HeartBeatDrivenDataStreamProgressReporterFixture.cs
+++ source/Halibut.Tests/Queue/QueuedDataStreams/HeartBeatDrivenDataStreamProgressReporterFixture.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,67 @@ namespace Halibut.Tests.Queue.QueuedDataStreams
     [TestFixture]
     public class HeartBeatDrivenDataStreamProgressReporterFixture
     {
-        
+        [Test]
+        public async Task HeartBeatReceived_WithSingleDataStream_CallsUpdateProgressAsyncWithPercentageComplete()
+        {
+            // Arrange
+            const int streamSize = 100;
+            var progressUpdates = new List<int>();
+            var progressUpdateCalls = 0;
+            
+            // Create a mock stream of size 100 bytes
+            var mockStream = new MemoryStream(new byte[streamSize]);
+            
+            // Create the updateProgressAsync function that captures progress updates
+            Task UpdateProgressAsync(int percentageComplete, CancellationToken ct)
+            {
+                progressUpdates.Add(percentageComplete);
+                Interlocked.Increment(ref progressUpdateCalls);
+                return Task.CompletedTask;
+            }
+            
+            // Create a DataStream using FromStream with our progress callback
+            var dataStream = DataStream.FromStream(mockStream, UpdateProgressAsync);
+            
+            // Create the HeartBeatDrivenDataStreamProgressReporter with our single DataStream
+            var progressReporter = HeartBeatDrivenDataStreamProgressReporter.CreateForDataStreams(new[] { dataStream });
+            
+            // Create heart beat messages with different progress values
+            var heartBeatMessage25 = new HeartBeatMessage
+            {
+                DataStreamProgress = new Dictionary<Guid, long>
+                {
+                    { dataStream.Id, 25 } // 25% complete (25 out of 100 bytes)
+                }
+            };
+            
+            var heartBeatMessage50 = new HeartBeatMessage
+            {
+                DataStreamProgress = new Dictionary<Guid, long>
+                {
+                    { dataStream.Id, 50 } // 50% complete (50 out of 100 bytes)
+                }
+            };
+            
+            var heartBeatMessage100 = new HeartBeatMessage
+            {
+                DataStreamProgress = new Dictionary<Guid, long>
+                {
+                    { dataStream.Id, 100 } // 100% complete (100 out of 100 bytes)
+                }
+            };
+            
+            // Act
+            await progressReporter.HeartBeatReceived(heartBeatMessage25, CancellationToken.None);
+            await progressReporter.HeartBeatReceived(heartBeatMessage50, CancellationToken.None);
+            await progressReporter.HeartBeatReceived(heartBeatMessage100, CancellationToken.None);
+            
+            // Assert
+            progressUpdateCalls.Should().Be(3, "updateProgressAsync should be called for each heart beat");
+            progressUpdates.Should().ContainInOrder(25, 50, 100);
+            
+            // Clean up
+            await progressReporter.DisposeAsync();
+        }
     }
 }
