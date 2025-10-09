bde9ae53fae8b4ca3568e989c98f72c632c91678
Luke Butters <luke.butters@octopus.com>
2025-10-09 14:26:07 +1100

    .

--- source/Halibut.Tests/Queue/Redis/Utils/JsonStoreDataStreamsForDistributedQueues.cs
+++ source/Halibut.Tests/Queue/Redis/Utils/JsonStoreDataStreamsForDistributedQueues.cs
@@ -28,7 +28,7 @@ namespace Halibut.Tests.Queue.Redis.Utils
 #if NET8_0_OR_GREATER
                     await dataStream.Receiver().ReadAsync(async (s, ct) => await s.CopyToAsync(memoryStream, ct), cancellationToken);
 #else
-                    await dataStream.Receiver().ReadAsync(async (s, ct) => await s.CopyToAsync(memoryStream), cancellationToken);
+                    throw new NotImplementedException("Redis PRQ is not supported in net48");
 #endif
                 }
                 else
