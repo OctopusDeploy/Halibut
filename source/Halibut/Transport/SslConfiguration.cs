d4d61d0e7d0e19d8f3e3f37cb70829b995243870
Rhys Parry <rhys.parry@octopus.com>
2025-10-22 09:00:12 +1000

    Default to legacy SSL configuration on NETFRAMEWORK

--- source/Halibut/Transport/SslConfiguration.cs
+++ source/Halibut/Transport/SslConfiguration.cs
@@ -2,6 +2,11 @@ namespace Halibut.Transport
 {
     public static class SslConfiguration
     {
-        public static ISslConfigurationProvider Default { get; } = new DefaultSslConfigurationProvider();
+        public static ISslConfigurationProvider Default { get; }
+#if NETFRAMEWORK
+            = new LegacySslConfigurationProvider();
+#else
+            = new DefaultSslConfigurationProvider();
+#endif
     }
 }
