ab47ddc178bc3499a5a49fdfee0a8f4eb726021a
Rhys Parry <rhys.parry@octopus.com>
2025-10-23 13:22:38 +1000

    Add comment for net4.8 divergence

--- source/Halibut/Transport/SslConfiguration.cs
+++ source/Halibut/Transport/SslConfiguration.cs
@@ -3,7 +3,7 @@ namespace Halibut.Transport
     public static class SslConfiguration
     {
         public static ISslConfigurationProvider Default { get; }
-#if NETFRAMEWORK
+#if NETFRAMEWORK // .NET4.8 exhibited inconsistent behavior when using the default configuration
             = new LegacySslConfigurationProvider();
 #else
             = new DefaultSslConfigurationProvider();
